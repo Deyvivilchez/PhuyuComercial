d6a5e57e64b2d9f8a1cfa0c52e841b6ba924cf21
Deyvivilchez <adcruzv@alumno.unsm.edu.pe>
2026-05-12 01:11:35 -0500

    migracion prod

--- phuyu/phuyu_form.js
+++ phuyu/phuyu_form.js
@@ -143,20 +143,22 @@ var phuyu_form = new Vue({
 			$(".compose").slideToggle();
 		},
 		np_guardar_cargarproductos: function(){
-            this.estado = 1; const formulario = new FormData($("#formulario_cargarproductos")[0]);
+            this.estado = 1; phuyu_sistema.phuyu_inicio_guardar("Subiendo archivo de productos, espere por favor...");
+            const self = this; const formulario = new FormData($("#formulario_cargarproductos")[0]);
             this.$http.post(url+"almacen/productos/cargarproductos", formulario).then(function (response) {
                 if (response.body.estado == 1) {
                     phuyu_sistema.phuyu_alerta("Guardado correctamente !!!", response.body.mensaje || "Carga del archivo realizada correctamente", "success");
                     if (typeof phuyu_datos !== "undefined" && phuyu_datos.phuyu_opcion) {
                         phuyu_datos.phuyu_opcion();
                     }
-                }else{
+                } else {
                     phuyu_sistema.phuyu_alerta("Operación no registrada !!!", response.body.mensaje || "Formato del archivo incorrecto", "error");
                 }
-                this.phuyu_cerrar(); this.estado = 0;
+                self.phuyu_cerrar(); self.estado = 0; phuyu_sistema.phuyu_fin();
             }, function(){
-				phuyu_sistema.phuyu_alerta("ESTAMOS TENIENDO PROBLEMAS", "ERROR DE RED","error");
-			});
+                phuyu_sistema.phuyu_alerta("ESTAMOS TENIENDO PROBLEMAS", "ERROR DE RED","error");
+                self.estado = 0; phuyu_sistema.phuyu_fin();
+            });
         },
         np_formato_cargarproductos: function(){
             window.open(url+"almacen/productos/formato_cargarproductos", "_blank");
@@ -165,20 +167,22 @@ var phuyu_form = new Vue({
             window.open(url+"almacen/productos/formato_stockextra", "_blank");
         },
         np_guardar_stockextra: function(){
-            this.estado = 1; const formulario = new FormData($("#formulario_stockextra")[0]);
+            this.estado = 1; phuyu_sistema.phuyu_inicio_guardar("Subiendo archivo de stock extra, espere por favor...");
+            const self = this; const formulario = new FormData($("#formulario_stockextra")[0]);
             this.$http.post(url+"almacen/productos/stockextra", formulario).then(function (response) {
                 if (response.body.estado == 1) {
                     phuyu_sistema.phuyu_alerta("Guardado correctamente !!!", response.body.mensaje || "Carga del archivo realizada correctamente", "success");
                     if (typeof phuyu_datos !== "undefined" && phuyu_datos.phuyu_opcion) {
                         phuyu_datos.phuyu_opcion();
                     }
-                }else{
+                } else {
                     phuyu_sistema.phuyu_alerta("Operación no registrada !!!", response.body.mensaje || "Formato del archivo incorrecto", "error");
                 }
-                this.phuyu_cerrar(); this.estado = 0;
+                self.phuyu_cerrar(); self.estado = 0; phuyu_sistema.phuyu_fin();
             }, function(){
-				phuyu_sistema.phuyu_alerta("ESTAMOS TENIENDO PROBLEMAS", "ERROR DE RED","error");
-			});
+                phuyu_sistema.phuyu_alerta("ESTAMOS TENIENDO PROBLEMAS", "ERROR DE RED","error");
+                self.estado = 0; phuyu_sistema.phuyu_fin();
+            });
         },
 		phuyu_marcar: function(){
 			if ($("#marcar").is(":checked")) {
